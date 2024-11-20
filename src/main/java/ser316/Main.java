eec4854275f939475531212f58f96712a5bf714e
Bhupinder Singh <bhupindersingh@Mac.attlocal.net>
2024-11-20 01:05:26 -0500

    Initial commit: Set up Gradle project with Checkstyle and SpotBugs integration

---
+++ src/main/java/ser316/Main.java
@@ -0,0 +1,7 @@
+package ser316;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Silicon Valley Simulator!");
+    }
+}
---
+++ src/test/java/ser316Test/TestPlaceholder.java
@@ -0,0 +1,12 @@
+package ser316Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestPlaceholder {
+    @Test
+    public void testPlaceholder() {
+        assertTrue(true);
+    }
+}
